cb7e0478b41c97862476a7122824756a500fd92a
dennis.sheirer@gmail.com <dennis.sheirer@gmail.com@bf34bd42-e16c-3238-7ec3-aff9568aef5a>
2014-10-20 23:25:37 +0000

    Updated HackRF controller to included GSG copyright notice.

--- trunk/src/source/tuner/hackrf/HackRFTunerController.java
+++ trunk/src/source/tuner/hackrf/HackRFTunerController.java
@@ -1,6 +1,59 @@
 package source.tuner.hackrf;
-
+/*******************************************************************************
+ *     SDR Trunk 
+ *     Copyright (C) 2014 Dennis Sheirer
+ *     
+ *     Ported from libhackrf at:
+ *     https://github.com/mossmann/hackrf/tree/master/host/libhackrf
+ *     
+ *	   Copyright (c) 2012, Jared Boone <jared@sharebrained.com>
+ *     Copyright (c) 2013, Benjamin Vernoux <titanmkd@gmail.com>
+ *     copyright (c) 2013, Michael Ossmann <mike@ossmann.com>
+ *     
+ *     All rights reserved.
+ *     
+ *     Redistribution and use in source and binary forms, with or without 
+ *     modification, are permitted provided that the following conditions are 
+ *     met:
+ *     
+ *     Redistributions of source code must retain the above copyright notice, 
+ *     this list of conditions and the following disclaimer.
+ *     Redistributions in binary form must reproduce the above copyright 
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *     
+ *     Neither the name of Great Scott Gadgets nor the names of its contributors 
+ *     may be used to endorse or promote products derived from this software
+ *     without specific prior written permission.
+ *     
+ *     THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+ *     "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED 
+ *     TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A 
+ *     PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT 
+ *     HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+ *     SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED 
+ *     TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
+ *     PROFITS; OR BUSINESS INTERRUPTION)
+ *     HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+ *     STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+ *     ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ *     POSSIBILITY OF SUCH DAMAGE.
+ *     
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>
+ ******************************************************************************/
 import java.nio.ByteBuffer;
+
 import java.nio.ByteOrder;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -31,6 +84,7 @@ import source.SourceException;
 import source.tuner.TunerConfiguration;
 import source.tuner.TunerController;
 import source.tuner.rtl.ByteSampleAdapter;
+
 
 public class HackRFTunerController extends TunerController
 {
@@ -291,6 +345,7 @@ public class HackRFTunerController extends TunerController
             {
 	            setSampleRate( hackRFConfig.getSampleRate() );
 	            setFrequencyCorrection( hackRFConfig.getFrequencyCorrection() );
+	            setAmplifierEnabled( hackRFConfig.getAmplifierEnabled() );
 	            setLNAGain( hackRFConfig.getLNAGain() );
 	            setVGAGain( hackRFConfig.getVGAGain() );
 	            setFrequency( getFrequency() );
