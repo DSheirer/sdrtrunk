129dba869ff82157d0e555e034f575a1d8dea32c
Denny <denny@denny-desktop>
2016-05-29 08:54:07 -0400

    Adds Remez filter designer for low, high and band-pass filters.  Support for Hilbert and Differentiator FIR filter designers may be added in the future.
    Adds developer filter viewer tool written in JavaFX to visualize the
    frequency and impulse response of FIR filters.  This application is not
    intended to be a user application and is specifically for the developer
    to analyze filters and filter specifications that will be used in the
    code.
    
    NOTE: Java 8 JDK is now required to build this project for version 0.3.x
    and forward.  The JavaFX visualizer application and future user
    interfaces will use the Java 8 version of JavaFX

---
+++ src/dsp/filter/design/FilterViewer.java
@@ -0,0 +1,96 @@
+package dsp.filter.design;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import dsp.filter.fir.FIRFilterSpecification;
+import dsp.filter.fir.remez.RemezFIRFilterDesigner;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class FilterViewer extends Application
+{
+	private final static Logger mLog = LoggerFactory.getLogger( FilterViewer.class );
+
+	/**
+	 * Developer tool to visualize filter designs
+	 */
+	public FilterViewer()
+	{
+	}
+	
+	@Override
+	public void start( Stage primaryStage ) throws Exception
+	{
+		Scene scene = new Scene( new FilterView( getFilter() ) );
+		
+		primaryStage.setTitle( "Filter Viewer" );
+		primaryStage.setScene( scene );
+		primaryStage.show();
+	}
+	
+	public static void main( String[] args )
+	{
+		launch( args );
+	}
+
+	/**
+	 * Provides the filter to visualize.  Modify this method to visualize your filter design.
+	 */
+	private float[] getFilter()
+	{
+		FIRFilterSpecification specification = FIRFilterSpecification.lowPassBuilder()
+		        .sampleRate( 10000 )
+		        .gridDensity( 16 )
+                .passBandCutoff( 1300 )
+		        .passBandAmplitude( 1.0 )
+		        .passBandRipple( 0.01 )
+		        .stopBandStart( 2675 )
+		        .stopBandAmplitude( 0.0 )
+		        .stopBandRipple( 0.001 )
+		        .build();
+		
+//		FIRFilterSpecification specification = FIRFilterSpecification.highPassBuilder()
+//		        .sampleRate( 8000 )
+//		        .gridDensity( 16 )
+//		        .stopBandCutoff( 2000 )
+//		        .stopBandAmplitude( 0.0 )
+//		        .stopBandRipple( 0.003 )
+//		        .passBandStart( 3000 )
+//		        .passBandAmplitude( 1.0 )
+//		        .passBandRipple( 0.001 )
+//		        .build();
+
+//		FIRFilterSpecification specification = FIRFilterSpecification.bandPassBuilder()
+//	        .sampleRate( 8000 )
+//	        .order( 164 )
+//	        .stopFrequency1( 1100 )
+//	        .passFrequencyBegin( 1200 )
+//	        .passFrequencyEnd( 2400 )
+//	        .stopFrequency2( 2500 )
+//	        .stopRipple( 0.01 )
+//	        .passRipple( 0.01 )
+//	        .build();
+
+		float[] taps = null;
+
+//		taps = FilterFactory.getLowPass( 16000, 2400, 73, WindowType.HANNING );
+
+		try
+		{
+			RemezFIRFilterDesigner designer = new RemezFIRFilterDesigner( specification );
+			
+			if( designer.isValid() )
+			{
+				taps = designer.getImpulseResponse();
+			}
+		}
+		catch( FilterDesignException fde )
+		{
+		    mLog.error( "Filter design error", fde );
+		}
+		
+		return taps;
+	}
+}
