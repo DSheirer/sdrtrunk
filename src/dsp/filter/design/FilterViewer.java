fce3d5c4f3dd5b89711b8360912898234d064268
Denny <denny@denny-desktop>
2017-02-11 07:17:23 -0500

    Resolves #186 - p25 C4FM decoder wasn't working (at all).

--- src/dsp/filter/design/FilterViewer.java
+++ src/dsp/filter/design/FilterViewer.java
@@ -1,65 +1,64 @@
 package dsp.filter.design;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import dsp.filter.fir.FIRFilterSpecification;
 import dsp.filter.fir.remez.RemezFIRFilterDesigner;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class FilterViewer extends Application
 {
-	private final static Logger mLog = LoggerFactory.getLogger( FilterViewer.class );
+    private final static Logger mLog = LoggerFactory.getLogger(FilterViewer.class);
 
-	/**
-	 * Developer tool to visualize filter designs
-	 */
-	public FilterViewer()
-	{
-	}
-	
-	@Override
-	public void start( Stage primaryStage ) throws Exception
-	{
-		Scene scene = new Scene( new FilterView( getFilter() ) );
-		
-		primaryStage.setTitle( "Filter Viewer" );
-		primaryStage.setScene( scene );
-		primaryStage.show();
-	}
-	
-	public static void main( String[] args )
-	{
-		launch( args );
-	}
+    /**
+     * Developer tool to visualize filter designs
+     */
+    public FilterViewer()
+    {
+    }
 
-	/**
-	 * Provides the filter to visualize.  Modify this method to visualize your filter design.
-	 */
-	private float[] getFilter()
-	{
-//		FIRFilterSpecification specification = FIRFilterSpecification.lowPassBuilder()
-//		        .sampleRate( 48000 )
-//		        .gridDensity( 16 )
-//                .passBandCutoff( 3200 )
-//		        .passBandAmplitude( 1.0 )
-//		        .passBandRipple( 0.02 )
-//		        .stopBandStart( 4000 )
-//		        .stopBandAmplitude( 0.0 )
-//		        .stopBandRipple( 0.03 )
-//		        .build();
-		
-		FIRFilterSpecification specification = FIRFilterSpecification.highPassBuilder()
-		        .sampleRate( 24000 )
-		        .stopBandCutoff( 800 )
-		        .stopBandAmplitude( 0.0 )
-		        .stopBandRipple( 0.03 )
-		        .passBandStart( 1000 )
-		        .passBandAmplitude( 1.0 )
-		        .passBandRipple( 0.08 )
-		        .build();
+    @Override
+    public void start(Stage primaryStage) throws Exception
+    {
+        Scene scene = new Scene(new FilterView(getFilter()));
+
+        primaryStage.setTitle("Filter Viewer");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+
+    /**
+     * Provides the filter to visualize.  Modify this method to visualize your filter design.
+     */
+    private float[] getFilter()
+    {
+        FIRFilterSpecification specification = FIRFilterSpecification.lowPassBuilder()
+            .sampleRate(48000)
+            .gridDensity(16)
+            .passBandCutoff(2500)
+            .passBandAmplitude(1.0)
+            .passBandRipple(0.01)
+            .stopBandStart(4000)
+            .stopBandAmplitude(0.0)
+            .stopBandRipple(0.008)
+            .build();
+
+//        FIRFilterSpecification specification = FIRFilterSpecification.highPassBuilder()
+//            .sampleRate(24000)
+//            .stopBandCutoff(800)
+//            .stopBandAmplitude(0.0)
+//            .stopBandRipple(0.03)
+//            .passBandStart(1000)
+//            .passBandAmplitude(1.0)
+//            .passBandRipple(0.08)
+//            .build();
 
 //		FIRFilterSpecification specification = FIRFilterSpecification.bandPassBuilder()
 //	        .sampleRate( 48000 )
@@ -71,24 +70,25 @@ public class FilterViewer extends Application
 //	        .passRipple( 0.008)
 //	        .build();
 
-		float[] taps = null;
+        float[] taps = null;
 
-//		taps = FilterFactory.getLowPass( 16000, 2400, 73, WindowType.HANNING );
+//        taps = FilterFactory.getLowPass(48000, 6750, 7500, 60,
+//            Window.WindowType.HAMMING, true);
 
-		try
-		{
-			RemezFIRFilterDesigner designer = new RemezFIRFilterDesigner( specification );
-			
-			if( designer.isValid() )
-			{
-				taps = designer.getImpulseResponse();
-			}
-		}
-		catch( FilterDesignException fde )
-		{
-		    mLog.error( "Filter design error", fde );
-		}
-		
-		return taps;
-	}
+        try
+        {
+            RemezFIRFilterDesigner designer = new RemezFIRFilterDesigner(specification);
+
+            if(designer.isValid())
+            {
+                taps = designer.getImpulseResponse();
+            }
+        }
+        catch(FilterDesignException fde)
+        {
+            mLog.error("Filter design error", fde);
+        }
+
+        return taps;
+    }
 }
