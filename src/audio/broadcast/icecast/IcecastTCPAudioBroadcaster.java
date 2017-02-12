c3d60dc1741d80965f3d88bb08f6bb42dfdde50e
Denny <denny@denny-desktop>
2017-02-12 08:04:27 -0500

    Resolves #193 - adds handling for MINA WriteTimeoutException error.  Resets connection when this occurs.

--- src/audio/broadcast/icecast/IcecastTCPAudioBroadcaster.java
+++ src/audio/broadcast/icecast/IcecastTCPAudioBroadcaster.java
@@ -25,6 +25,7 @@ import org.apache.mina.core.RuntimeIoException;
 import org.apache.mina.core.future.ConnectFuture;
 import org.apache.mina.core.service.IoHandlerAdapter;
 import org.apache.mina.core.session.IoSession;
+import org.apache.mina.core.write.WriteTimeoutException;
 import org.apache.mina.filter.codec.ProtocolCodecFilter;
 import org.apache.mina.transport.socket.nio.NioSocketConnector;
 import org.slf4j.Logger;
@@ -272,6 +273,12 @@ public class IcecastTCPAudioBroadcaster extends IcecastAudioBroadcaster
                         disconnect();
                         mLog.error("Unrecognized IO error: " + reason + ". Streaming halted.", cause);
                     }
+                }
+                else if(ioe instanceof WriteTimeoutException)
+                {
+                    mLog.info("Network write timeout error - resetting connection");
+                    disconnect();
+                    connect();
                 }
                 else
                 {
