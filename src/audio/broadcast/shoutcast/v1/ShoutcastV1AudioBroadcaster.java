79b4b30ec7f22d2501a6e5dfb06696c6569cc4e2
Denny <denny@denny-desktop>
2017-02-17 17:53:42 -0500

    Resolves issue with audio broadcasters attempting to reconnect if a connection attempt is not successful.

--- src/audio/broadcast/shoutcast/v1/ShoutcastV1AudioBroadcaster.java
+++ src/audio/broadcast/shoutcast/v1/ShoutcastV1AudioBroadcaster.java
@@ -168,6 +168,8 @@ public class ShoutcastV1AudioBroadcaster extends AudioBroadcaster
 
                         disconnect();
                     }
+
+                    mConnecting.set(false);
                 }
             };
 
