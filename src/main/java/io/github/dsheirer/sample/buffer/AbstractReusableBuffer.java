e7b592141f9e97ac35e76dccfa1c2c5f5c287e87
billh <whelgeson@@users.noreply.github.com>
2018-12-14 21:57:00 -0600

    Fix for issue #441. Method doc reads that the method is thread-safe, but there exist no locking to ensure thread safety on call into recycle method. Modifying the decrementUserCount method to block other callers to ensure thread-safe access.
    This appears to be a hot area of code and there may be a performance hit by doing this, please double check my work here.

--- src/main/java/io/github/dsheirer/sample/buffer/AbstractReusableBuffer.java
+++ src/main/java/io/github/dsheirer/sample/buffer/AbstractReusableBuffer.java
@@ -93,7 +93,7 @@ public abstract class AbstractReusableBuffer
      *
      * This method is thread-safe.
      */
-    public void decrementUserCount()
+    public synchronized void decrementUserCount()
     {
         if(mUserCount.decrementAndGet() <= 0)
         {
