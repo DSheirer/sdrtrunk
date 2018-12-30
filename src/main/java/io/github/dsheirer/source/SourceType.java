5a963b21aca8e7d41ea08c6b2b9e447f58921675
Denny <dennissheirer2002@yahoo.com>
2018-12-30 13:06:27 +0000

    Initial work on add support for rotating control channels.

--- src/main/java/io/github/dsheirer/source/SourceType.java
+++ src/main/java/io/github/dsheirer/source/SourceType.java
@@ -1,54 +1,51 @@
 /*******************************************************************************
- *     SDR Trunk 
- *     Copyright (C) 2014 Dennis Sheirer
- * 
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- * 
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
- * 
- *     You should have received a copy of the GNU General Public License
- *     along with this program.  If not, see <http://www.gnu.org/licenses/>
+ * sdr-trunk
+ * Copyright (C) 2014-2018 Dennis Sheirer
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by  the Free Software Foundation, either version 3 of the License, or  (at your option) any
+ * later version.
+ *
+ * This program is distributed in the hope that it will be useful,  but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License  along with this program.
+ * If not, see <http://www.gnu.org/licenses/>
+ *
  ******************************************************************************/
 package io.github.dsheirer.source;
 
-
+/**
+ * Source Configuration Types enumeration.
+ */
 public enum SourceType
 {
-	NONE( "No Source" ),
-	MIXER( "Sound Card" ),
-	TUNER( "Tuner" ),
-	RECORDING( "IQ Recording" );
-	
+    NONE("No Source"),
+    MIXER("Sound Card"),
+    TUNER("Tuner"),
+    TUNER_MULTIPLE_FREQUENCIES("Tuner - Multiple Frequencies"),
+    RECORDING("IQ Recording");
+
     private String mDisplayString;
-    
-    SourceType( String displayString )
+
+    SourceType(String displayString)
     {
         mDisplayString = displayString;
     }
-    
+
     public static SourceType[] getTypes()
     {
-    	SourceType[] types = new SourceType[ 2 ];
-    	types[ 0 ] = SourceType.TUNER;
-    	types[ 1 ] = SourceType.MIXER;
+        SourceType[] types = new SourceType[3];
+        types[0] = SourceType.TUNER;
+        types[1] = SourceType.TUNER_MULTIPLE_FREQUENCIES;
+        types[2] = SourceType.MIXER;
 
-    	return types;
+        return types;
     }
-    
-    public String getDisplayString()
-    {
-        return mDisplayString;
-    }
-    
+
     @Override
     public String toString()
     {
-    	return mDisplayString;
+        return mDisplayString;
     }
 }
---
+++ src/main/java/io/github/dsheirer/source/config/SourceConfigTunerMultipleFrequency.java
@@ -0,0 +1,102 @@
+/*******************************************************************************
+ * sdr-trunk
+ * Copyright (C) 2014-2018 Dennis Sheirer
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by  the Free Software Foundation, either version 3 of the License, or  (at your option) any
+ * later version.
+ *
+ * This program is distributed in the hope that it will be useful,  but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License  along with this program.
+ * If not, see <http://www.gnu.org/licenses/>
+ *
+ ******************************************************************************/
+package io.github.dsheirer.source.config;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import io.github.dsheirer.source.SourceType;
+import io.github.dsheirer.source.tuner.channel.TunerChannel;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Multiple frequency tuner source configuration is used for a system with a rolling/rotating control
+ * channel where the control channel rotates across a pre-defined set of channel frequencies.
+ */
+@JsonSubTypes.Type(value = SourceConfigTunerMultipleFrequency.class, name = "sourceConfigTunerMultipleFrequency")
+public class SourceConfigTunerMultipleFrequency extends SourceConfiguration
+{
+    private List<Long> mFrequencies = new ArrayList<>();
+    private String mPreferredTuner;
+
+    public SourceConfigTunerMultipleFrequency()
+    {
+        super(SourceType.TUNER_MULTIPLE_FREQUENCIES);
+    }
+
+    /**
+     * List of frequencies for this configuration
+     */
+    @JacksonXmlProperty(isAttribute = false, localName = "frequency")
+    public List<Long> getFrequencies()
+    {
+        return mFrequencies;
+    }
+
+    /**
+     * Sets the list of frequencies for this configuration
+     */
+    public void setFrequencies(List<Long> frequencies)
+    {
+        mFrequencies = frequencies;
+    }
+
+    /**
+     * Adds a frequency to the list
+     */
+    public void addFrequency(long frequency)
+    {
+        mFrequencies.add(frequency);
+    }
+
+    /**
+     * Preferred tuner to use for this configuration.
+     * @return tuner name or null
+     */
+    @JacksonXmlProperty(isAttribute = true, localName = "preferred_tuner")
+    public String getPreferredTuner()
+    {
+        return mPreferredTuner;
+    }
+
+    /**
+     * Indicates if this configuration has a specified preferred tuner
+     */
+    @JsonIgnore
+    public boolean hasPreferredTuner()
+    {
+        return mPreferredTuner != null;
+    }
+
+    /**
+     * Specifies the preferred tuner to use for this configuration
+     * @param preferredTuner to use, if available
+     */
+    public void setPreferredTuner(String preferredTuner)
+    {
+        mPreferredTuner = preferredTuner;
+    }
+
+    @JsonIgnore
+    @Override
+    public String getDescription()
+    {
+        return "Frequencies [" + mFrequencies.size() + "]";
+    }
+}
