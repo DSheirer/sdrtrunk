d601cbda0e739f8e5b1ac087613d6f08a59bbb58
Denny Sheirer <dennissheirer2002@yahoo.com>
2019-01-26 11:11:41 +0000

    Resolves issue with missing case statements in talkgroup format classes. (#509)

--- src/main/java/io/github/dsheirer/preference/identifier/talkgroup/PassportTalkgroupFormatter.java
+++ src/main/java/io/github/dsheirer/preference/identifier/talkgroup/PassportTalkgroupFormatter.java
@@ -38,6 +38,7 @@ public class PassportTalkgroupFormatter extends IntegerFormatter
             switch(format)
             {
                 case DECIMAL:
+                case FORMATTED:
                     return toDecimal(identifier.getValue(), DECIMAL_WIDTH);
                 case HEXADECIMAL:
                     return toHex(identifier.getValue(), HEXADECIMAL_WIDTH);
