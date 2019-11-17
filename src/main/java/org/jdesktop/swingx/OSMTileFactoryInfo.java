b74f12ace4a9f9fe4af2fc6a2e72d2f5a6290fcd
Denny <denny@denny-desktop>
2019-11-17 11:39:07 -0500

    Resolves #570 map tile data not downloading.  Also resolves issue with the default map loading icon pointing to an old image reference.

--- src/main/java/org/jdesktop/swingx/OSMTileFactoryInfo.java
+++ src/main/java/org/jdesktop/swingx/OSMTileFactoryInfo.java
@@ -1,28 +1,24 @@
-/*******************************************************************************
- *     SDR Trunk 
- *     Copyright (C) 2014 Dennis Sheirer
- * 
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- * 
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
- * 
- *     You should have received a copy of the GNU General Public License
- *     along with this program.  If not, see <http://www.gnu.org/licenses/>
- ******************************************************************************/
-// Fraunhofer Institute for Computer Graphics Research (IGD)
-// Department Information Visualization and Visual Analytics
-//
-// Copyright (c) Fraunhofer IGD. All rights reserved.
-//
-// This source code is property of the Fraunhofer IGD and underlies
-// copyright restrictions. It may only be used with explicit
-// permission from the respective owner.
+/*
+ *
+ *  * ******************************************************************************
+ *  * Copyright (C) 2014-2019 Dennis Sheirer
+ *  *
+ *  * This program is free software: you can redistribute it and/or modify
+ *  * it under the terms of the GNU General Public License as published by
+ *  * the Free Software Foundation, either version 3 of the License, or
+ *  * (at your option) any later version.
+ *  *
+ *  * This program is distributed in the hope that it will be useful,
+ *  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  * GNU General Public License for more details.
+ *  *
+ *  * You should have received a copy of the GNU General Public License
+ *  * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *  * *****************************************************************************
+ *
+ *
+ */
 
 package org.jdesktop.swingx;
 
@@ -44,7 +40,7 @@ public class OSMTileFactoryInfo extends TileFactoryInfo
 		super("OpenStreetMap", 
 				1, max - 2, max, 
 				256, true, true, 					// tile size is 256 and x/y orientation is normal
-				"http://tile.openstreetmap.org",
+				"https://tile.openstreetmap.org",
 				"x", "y", "z");						// 5/15/10.png
 	}
 
