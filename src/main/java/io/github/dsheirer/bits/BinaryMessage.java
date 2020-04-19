554b0e5c0266145b33886596b586406e616e4a48
Rob Ekl <xwq876@zebra.com>
2020-04-19 16:35:38 -0500

    Include other member variables in copy constructor

--- src/main/java/io/github/dsheirer/bits/BinaryMessage.java
+++ src/main/java/io/github/dsheirer/bits/BinaryMessage.java
@@ -100,6 +100,8 @@ public class BinaryMessage extends BitSet
         this(toCopyFrom.size());
         this.or(toCopyFrom);
         this.mPointer = toCopyFrom.pointer();
+        this.mCRC = toCopyFrom.mCRC;
+        this.mSize = toCopyFrom.mSize;
     }
 
     public BinaryMessage(BitSet bitset, int size)
