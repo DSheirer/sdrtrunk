8b487d79e4d14a8d695e983fe35ad779746d352a
Denny Sheirer <dennissheirer2002@yahoo.com>
2021-11-15 04:20:08 -0500

    Adds Comparable to identifiers that are used in TreeSets. (#1103)
    Co-authored-by: Dennis Sheirer <dsheirer@github.com>

--- src/main/java/io/github/dsheirer/module/decode/mdc1200/identifier/MDC1200Identifier.java
+++ src/main/java/io/github/dsheirer/module/decode/mdc1200/identifier/MDC1200Identifier.java
@@ -1,23 +1,20 @@
 /*
+ * *****************************************************************************
+ * Copyright (C) 2014-2021 Dennis Sheirer
  *
- *  * ******************************************************************************
- *  * Copyright (C) 2014-2019 Dennis Sheirer
- *  *
- *  * This program is free software: you can redistribute it and/or modify
- *  * it under the terms of the GNU General Public License as published by
- *  * the Free Software Foundation, either version 3 of the License, or
- *  * (at your option) any later version.
- *  *
- *  * This program is distributed in the hope that it will be useful,
- *  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  * GNU General Public License for more details.
- *  *
- *  * You should have received a copy of the GNU General Public License
- *  * along with this program.  If not, see <http://www.gnu.org/licenses/>
- *  * *****************************************************************************
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ * ****************************************************************************
  */
 
 package io.github.dsheirer.module.decode.mdc1200.identifier;
@@ -29,7 +26,7 @@ import io.github.dsheirer.protocol.Protocol;
 /**
  * Identifier for an MDC1200 radio identity
  */
-public class MDC1200Identifier extends TalkgroupIdentifier
+public class MDC1200Identifier extends TalkgroupIdentifier implements Comparable<MDC1200Identifier>
 {
 
     /**
@@ -60,5 +57,11 @@ public class MDC1200Identifier extends TalkgroupIdentifier
     public static MDC1200Identifier createFrom(int value)
     {
         return new MDC1200Identifier(value, Role.FROM);
+    }
+
+    @Override
+    public int compareTo(MDC1200Identifier other)
+    {
+        return Integer.compare(getValue(), other != null ? other.getValue() : 0);
     }
 }
