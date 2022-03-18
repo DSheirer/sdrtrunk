6d2af95e9a79b02b16e2e7bec5a3cfaf4a7fe1af
Denny Sheirer <dennissheirer2002@yahoo.com>
2022-03-18 07:00:19 -0400

    #1173 updates DMR decoder to support DCDM (#1174)
    Co-authored-by: Dennis Sheirer <dsheirer@github.com>

--- src/main/java/io/github/dsheirer/module/decode/dmr/DMRDecoderState.java
+++ src/main/java/io/github/dsheirer/module/decode/dmr/DMRDecoderState.java
@@ -1,23 +1,20 @@
 /*
+ * *****************************************************************************
+ * Copyright (C) 2014-2022 Dennis Sheirer
  *
- *  * ******************************************************************************
- *  * Copyright (C) 2014-2020 Dennis Sheirer, Zhenyu Mao
- *  *
- *  * This program is free software: you can redistribute it and/or modify
- *  * it under the terms of the GNU General Public License as published by
- *  * the Free Software Foundation, either version 3 of the License, or
- *  * (at your option) any later version.
- *  *
- *  * This program is distributed in the hope that it will be useful,
- *  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  * GNU General Public License for more details.
- *  *
- *  * You should have received a copy of the GNU General Public License
- *  * along with this program.  If not, see <http://www.gnu.org/licenses/>
- *  * *****************************************************************************
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ * ****************************************************************************
  */
 package io.github.dsheirer.module.decode.dmr;
 
@@ -40,6 +37,7 @@ import io.github.dsheirer.message.IMessage;
 import io.github.dsheirer.module.decode.DecoderType;
 import io.github.dsheirer.module.decode.dmr.channel.DMRChannel;
 import io.github.dsheirer.module.decode.dmr.event.DMRDecodeEvent;
+import io.github.dsheirer.module.decode.dmr.identifier.DMRTalkgroup;
 import io.github.dsheirer.module.decode.dmr.message.DMRMessage;
 import io.github.dsheirer.module.decode.dmr.message.data.DataMessage;
 import io.github.dsheirer.module.decode.dmr.message.data.csbk.CSBKMessage;
@@ -307,7 +305,17 @@ public class DMRDecoderState extends TimeslotDecoderState
      */
     private void processVoice(VoiceMessage message)
     {
-        broadcast(new DecoderStateEvent(this, Event.CONTINUATION, State.CALL, getTimeslot()));
+        if(message.getSyncPattern().isDirectMode())
+        {
+            updateCurrentCall(DecodeEventType.CALL, "DIRECT MODE", message.getTimestamp());
+
+            //Use the timeslot as the talkgroup identifier since DCDM mode doesn't use talkgroups
+            getIdentifierCollection().update(DMRTalkgroup.create(getTimeslot()));
+        }
+        else
+        {
+            broadcast(new DecoderStateEvent(this, Event.CONTINUATION, State.CALL, getTimeslot()));
+        }
     }
 
     /**
--- src/main/java/io/github/dsheirer/module/decode/dmr/message/voice/VoiceEMBMessage.java
+++ src/main/java/io/github/dsheirer/module/decode/dmr/message/voice/VoiceEMBMessage.java
@@ -1,3 +1,22 @@
+/*
+ * *****************************************************************************
+ * Copyright (C) 2014-2022 Dennis Sheirer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ * ****************************************************************************
+ */
+
 package io.github.dsheirer.module.decode.dmr.message.voice;
 
 import io.github.dsheirer.bits.BinaryMessage;
@@ -37,25 +56,14 @@ public class VoiceEMBMessage extends VoiceMessage
 
         if(getEMB().isValid())
         {
-            sb.append("CC:").append(getEMB().getColorCode());
-        }
-        else
-        {
-            sb.append("CC:?");
+            sb.append("CC:").append(getEMB().getColorCode()).append(" ");
         }
 
-        sb.append(" ").append(getSyncPattern().toString());
+        sb.append(getSyncPattern().toString());
 
-        if(getEMB().isValid())
+        if(getEMB().isValid() && getEMB().isEncrypted())
         {
-            if(getEMB().isEncrypted())
-            {
-                sb.append(" ENCRYPTED");
-            }
-        }
-        else
-        {
-            sb.append(" [EMB CRC-ERROR]");
+            sb.append(" ENCRYPTED");
         }
 
         return sb.toString();
