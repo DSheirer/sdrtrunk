9492ec7dab654c77217f596fa6aaf1db1b3512b8
Andrzej Pańkowski <github@irratio.nl>
2022-08-02 15:44:22 +0200

    Unit test for Identifier Collection constructor

--- src/test/java/io/github/dsheirer/identifier/IdentifierCollectionTest.java
+++ src/test/java/io/github/dsheirer/identifier/IdentifierCollectionTest.java
@@ -1,6 +1,7 @@
 package io.github.dsheirer.identifier;
 
 import io.github.dsheirer.CommonFixtures;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -12,5 +13,18 @@ public class IdentifierCollectionTest implements CommonFixtures {
     // expect
     assertThat(someIdentifiers()).isEqualTo(someIdentifiers());
     assertThat(someIdentifiers().hashCode()).isEqualTo(someIdentifiers().hashCode());
+  }
+
+  @Test
+  public void shouldSetIdentifiersAndTimeslot() {
+    // given
+    final var identifiers = someIdentifiers().mIdentifiers;
+    final var collection = new IdentifierCollection(identifiers, 8);
+
+    // expect
+    final var softly = new SoftAssertions();
+    softly.assertThat(collection.mIdentifiers).isEqualTo(identifiers);
+    softly.assertThat(collection.getTimeslot()).isEqualTo(8);
+    softly.assertAll();
   }
 }
