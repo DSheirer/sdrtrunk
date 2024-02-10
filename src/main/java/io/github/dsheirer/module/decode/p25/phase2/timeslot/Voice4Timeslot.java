f6c4cc8a5f35bb7e67d1f7c7cb1d15774724a4cc
Denny Sheirer <dennissheirer2002@yahoo.com>
2024-02-10 03:40:57 -0500

    1810 P25 Voice frame list concurrent modification error.  Added synchronized to method to deconflict when the audio module or the binary frame recorder can access the message and not step on each other.  Issue was noted in the phase 2 module, but update the phase 1 module as well to protect against the issue. (#1833)
    Co-authored-by: Dennis Sheirer <dsheirer@github.com>

--- src/main/java/io/github/dsheirer/module/decode/p25/phase2/timeslot/Voice4Timeslot.java
+++ src/main/java/io/github/dsheirer/module/decode/p25/phase2/timeslot/Voice4Timeslot.java
@@ -1,23 +1,20 @@
 /*
+ * *****************************************************************************
+ * Copyright (C) 2014-2024 Dennis Sheirer
  *
- *  * ******************************************************************************
- *  * Copyright (C) 2014-2019 Dennis Sheirer
- *  *
- *  * This program is free software: you can redistribute it and/or modify
- *  * it under the terms of the GNU General Public License as published by
- *  * the Free Software Foundation, either version 3 of the License, or
- *  * (at your option) any later version.
- *  *
- *  * This program is distributed in the hope that it will be useful,
- *  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  * GNU General Public License for more details.
- *  *
- *  * You should have received a copy of the GNU General Public License
- *  * along with this program.  If not, see <http://www.gnu.org/licenses/>
- *  * *****************************************************************************
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ * ****************************************************************************
  */
 
 package io.github.dsheirer.module.decode.p25.phase2.timeslot;
@@ -25,7 +22,6 @@ package io.github.dsheirer.module.decode.p25.phase2.timeslot;
 import io.github.dsheirer.bits.BinaryMessage;
 import io.github.dsheirer.bits.CorrectedBinaryMessage;
 import io.github.dsheirer.module.decode.p25.phase2.enumeration.DataUnitID;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -62,7 +58,7 @@ public class Voice4Timeslot extends AbstractVoiceTimeslot
     /**
      * Voice frames contained in this timeslot
      */
-    public List<BinaryMessage> getVoiceFrames()
+    public synchronized List<BinaryMessage> getVoiceFrames()
     {
         if(mVoiceFrames == null)
         {
@@ -71,7 +67,6 @@ public class Voice4Timeslot extends AbstractVoiceTimeslot
             mVoiceFrames.add(getMessage().getSubMessage(FRAME_2_START, FRAME_2_START + FRAME_LENGTH));
             mVoiceFrames.add(getMessage().getSubMessage(FRAME_3_START, FRAME_3_START + FRAME_LENGTH));
             mVoiceFrames.add(getMessage().getSubMessage(FRAME_4_START, FRAME_4_START + FRAME_LENGTH));
-            return mVoiceFrames;
         }
 
         return mVoiceFrames;
