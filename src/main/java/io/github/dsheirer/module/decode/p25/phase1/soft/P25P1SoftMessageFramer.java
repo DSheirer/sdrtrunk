2dc3cf498ebfaf18e6a480538f2df4c875fa1412
Dennis Sheirer <dsheirer@github.com>
2024-12-25 08:03:41 -0500

    #1067 WIP P25P1 DQPSK decoder redesign.

---
+++ src/main/java/io/github/dsheirer/module/decode/p25/phase1/soft/P25P1SoftMessageFramer.java
@@ -0,0 +1,104 @@
+/*
+ * *****************************************************************************
+ * Copyright (C) 2014-2024 Dennis Sheirer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ * ****************************************************************************
+ */
+
+package io.github.dsheirer.module.decode.p25.phase1.soft;
+
+import io.github.dsheirer.dsp.symbol.Dibit;
+import io.github.dsheirer.message.IMessage;
+import io.github.dsheirer.module.decode.p25.phase1.P25P1DataUnitID;
+import io.github.dsheirer.sample.Listener;
+
+public class P25P1SoftMessageFramer implements Listener<Dibit>
+{
+    private Listener<IMessage> mMessageListener;
+    private boolean mRunning = false;
+    private int mDibitCounter = 0;
+    private int mDibitSinceTimestampCounter = 0;
+    private long mReferenceTimestamp = 0;
+    private P25P1DataUnitID mDetectedDUID = P25P1DataUnitID.UNKNOWN;
+
+    @Override
+    public void receive(Dibit dibit)
+    {
+    }
+
+    /**
+     * External trigger that a sync pattern is detected and the next arriving dibit is the start of the message that
+     * contains that sync.
+     */
+    public void syncDetected(int nac, P25P1DataUnitID dataUnitID)
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.append("NAC: ").append(nac);
+        sb.append(" Data Unit: ").append(dataUnitID);
+        System.out.println(sb);
+    }
+
+    /**
+     * Starts this framer dispatching messages
+     */
+    public void start()
+    {
+        mRunning = true;
+    }
+
+    /**
+     * Stops this framer from dispatching messages
+     */
+    public void stop()
+    {
+        mRunning = false;
+    }
+
+    /**
+     * Sets the listener to receive framed DMR messages.
+     * @param listener for messages.
+     */
+    public void setListener(Listener<IMessage> listener)
+    {
+        mMessageListener = listener;
+    }
+
+    /**
+     * Sets or updates the current dibit stream time from an incoming sample buffer.
+     * @param time to use as a reference timestamp.
+     */
+    public void setTimestamp(long time)
+    {
+        mReferenceTimestamp = time;
+        mDibitSinceTimestampCounter = 0;
+    }
+
+    /**
+     * Calculates the timestamp accurate to the currently received dibit.
+     * @return timestamp in milliseconds.
+     */
+    private long getTimestamp()
+    {
+        if(mReferenceTimestamp > 0)
+        {
+            return mReferenceTimestamp + (long)(1000.0 * mDibitSinceTimestampCounter / 4800);
+        }
+        else
+        {
+            mDibitSinceTimestampCounter = 0;
+            return System.currentTimeMillis();
+        }
+    }
+}
