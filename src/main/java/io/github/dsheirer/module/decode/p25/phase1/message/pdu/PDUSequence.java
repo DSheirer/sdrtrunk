758794fdb4ec47b87f1b76ef411db7705935c60b
Denny Sheirer <dennissheirer2002@yahoo.com>
2025-03-21 07:54:47 -0400

    #2195 Fix P25P1 NPE when accessing AMBTC network status message data blocks. (#2204)
    Co-authored-by: Dennis Sheirer <dsheirer@github.com>

--- src/main/java/io/github/dsheirer/module/decode/p25/phase1/message/pdu/PDUSequence.java
+++ src/main/java/io/github/dsheirer/module/decode/p25/phase1/message/pdu/PDUSequence.java
@@ -1,7 +1,6 @@
 /*
- * ******************************************************************************
- * sdrtrunk
- * Copyright (C) 2014-2019 Dennis Sheirer
+ * *****************************************************************************
+ * Copyright (C) 2014-2025 Dennis Sheirer
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,13 +14,12 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
- * *****************************************************************************
+ * ****************************************************************************
  */
 package io.github.dsheirer.module.decode.p25.phase1.message.pdu;
 
 import io.github.dsheirer.message.IBitErrorProvider;
 import io.github.dsheirer.module.decode.p25.phase1.message.pdu.block.DataBlock;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -74,7 +72,10 @@ public class PDUSequence implements IBitErrorProvider
      */
     public void addDataBlock(DataBlock dataBlock)
     {
-        mDataBlocks.add(dataBlock);
+        if(dataBlock != null)
+        {
+            mDataBlocks.add(dataBlock);
+        }
     }
 
     public boolean hasDataBlock(int index)
